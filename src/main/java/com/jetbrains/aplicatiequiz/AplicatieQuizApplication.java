fad3a169329c635e94c2d18869bf714b4f874d9f
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-21 12:21:33 +0300

    initial commit
    initial commit

---
+++ src/main/java/com/jetbrains/aplicatiequiz/AplicatieQuizApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.aplicatiequiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicatieQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicatieQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/aplicatiequiz/AplicatieQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.aplicatiequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicatieQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
