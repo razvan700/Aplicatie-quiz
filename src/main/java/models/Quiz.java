846af68c11f37ead8be4dbbd0a6d2184803fcbd0
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-21 13:47:45 +0300

    Implemented quiz class

--- src/main/java/models/Quiz.java
+++ src/main/java/models/Quiz.java
@@ -1,7 +1,19 @@
 package models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<Question> questions = new ArrayList<>();
 }
