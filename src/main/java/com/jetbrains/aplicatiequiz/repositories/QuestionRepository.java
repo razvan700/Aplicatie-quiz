258b34a65b5e00f2614aad0f12e7b83d4899e628
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-21 23:39:48 +0300

    Added question repository

---
+++ src/main/java/com/jetbrains/aplicatiequiz/repositories/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.jetbrains.aplicatiequiz.repositories;
+
+import com.jetbrains.aplicatiequiz.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question,Long> {
+}
--- src/main/java/com/jetbrains/aplicatiequiz/services/QuestionService.java
+++ src/main/java/com/jetbrains/aplicatiequiz/services/QuestionService.java
@@ -1,6 +1,18 @@
 package com.jetbrains.aplicatiequiz.services;
 
+import com.jetbrains.aplicatiequiz.models.Question;
+
+import java.util.List;
+
 public interface QuestionService {
 
+    Question createQuestion(Long quizId, Question question);
 
+    List<Question> getQuestionsByQuiz(Long quizId);
+
+    Question getQuestion(Long id);
+
+    Question updateQuestion(Long id, Question question);
+
+    boolean deleteQuestion(Long id);
 }
