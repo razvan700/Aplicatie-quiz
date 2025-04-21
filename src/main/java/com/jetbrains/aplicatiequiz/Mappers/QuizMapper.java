b364837b1e68ffd3ecd54f85b5fc027db8253ce1
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-21 15:45:52 +0300

    Implemented Quiz DTO mapper

--- src/main/java/com/jetbrains/aplicatiequiz/Mappers/QuizMapper.java
+++ src/main/java/com/jetbrains/aplicatiequiz/Mappers/QuizMapper.java
@@ -1,8 +1,25 @@
 package com.jetbrains.aplicatiequiz.Mappers;
 
+import com.jetbrains.aplicatiequiz.dto.QuizDTO;
+import com.jetbrains.aplicatiequiz.models.Quiz;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QuizMapper {
 
+    public QuizDTO toDTO(Quiz quiz){
+        QuizDTO dto = new QuizDTO();
+        dto.setId(quiz.getId());
+        dto.setQuestions(quiz.getQuestions());
+        dto.setTitle(quiz.getTitle());
+        return dto;
+    }
+
+    public Quiz toEntity(QuizDTO dto){
+        Quiz quiz = new Quiz();
+        quiz.setId(dto.getId());
+        quiz.setQuestions(dto.getQuestions());
+        quiz.setTitle(dto.getTitle());
+        return quiz;
+    }
 }
