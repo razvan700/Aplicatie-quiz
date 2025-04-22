3407bdc1ce998221a276d43a3fd0af60291865ae
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-22 19:44:13 +0300

    Added question service

--- src/main/java/com/jetbrains/aplicatiequiz/repositories/QuestionRepository.java
+++ src/main/java/com/jetbrains/aplicatiequiz/repositories/QuestionRepository.java
@@ -4,4 +4,5 @@ import com.jetbrains.aplicatiequiz.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
+
 }
---
+++ src/main/java/com/jetbrains/aplicatiequiz/services/QuestionServiceImpl.java
@@ -0,0 +1,56 @@
+package com.jetbrains.aplicatiequiz.services;
+
+import com.jetbrains.aplicatiequiz.models.Question;
+import com.jetbrains.aplicatiequiz.repositories.QuestionRepository;
+import com.jetbrains.aplicatiequiz.repositories.QuizRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+@Transactional
+public class QuestionServiceImpl implements QuestionService {
+
+    private final Logger logger = Logger.getLogger(QuestionServiceImpl.class.getName());
+
+    private final QuizRepository quizRepository;
+
+    private final QuestionRepository questionRepository;
+
+    public QuestionServiceImpl(QuizRepository quizRepository,
+                               QuestionRepository questionRepository) {
+        this.quizRepository = quizRepository;
+        this.questionRepository = questionRepository;
+    }
+
+    @Override
+    public Question createQuestion(Long quizId, Question question) {
+        return null;
+    }
+
+    @Override
+    public List<Question> getQuestionsByQuiz(Long quizId) {
+        logger.info("Fetching quiz by ID: " + quizId);
+//        return questionRepository.findAllByQuizId(quizId)
+//                .orElseThrow(() -> new RuntimeException("Quiz not found with ID: " + quizId));
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Question getQuestion(Long id) {
+        return null;
+    }
+
+    @Override
+    public Question updateQuestion(Long id, Question question) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteQuestion(Long id) {
+        return false;
+    }
+}
