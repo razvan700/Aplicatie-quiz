e82fffecc0a9682efef6705934a0ef208fd7bdc7
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-23 12:26:30 +0300

    Added find by quiz id method

--- src/main/java/com/jetbrains/aplicatiequiz/repositories/QuestionRepository.java
+++ src/main/java/com/jetbrains/aplicatiequiz/repositories/QuestionRepository.java
@@ -3,6 +3,10 @@ package com.jetbrains.aplicatiequiz.repositories;
 import com.jetbrains.aplicatiequiz.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 
+    List<Question> findByQuizId(Long quizId);
+    boolean existsByQuizId(Long quizId);
 }
